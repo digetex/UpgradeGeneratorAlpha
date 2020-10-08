65f41d6a0f2b6dbd73f141fbbb39a7ad202ca624
digetex <dagedigetex@GMAIL.COM>
2020-10-08 20:49:40 +0300

    interface

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,80 @@
 package sample;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.image.ImageView;
+
 public class Controller {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private ImageView Image;
+
+    @FXML
+    private RadioButton FirstGr_first;
+
+    @FXML
+    private ToggleGroup FirstUp;
+
+    @FXML
+    private RadioButton FirstGr_second;
+
+    @FXML
+    private RadioButton FirstGr_third;
+
+    @FXML
+    private RadioButton SecondGr_first;
+
+    @FXML
+    private ToggleGroup SecondUp;
+
+    @FXML
+    private RadioButton SecondGr_second;
+
+    @FXML
+    private RadioButton SecondGr_third;
+
+    @FXML
+    private RadioButton ThirdGr_first;
+
+    @FXML
+    private ToggleGroup ThirdUp;
+
+    @FXML
+    private RadioButton ThirdGr_second;
+
+    @FXML
+    private RadioButton ThirdGr_third;
+
+    @FXML
+    private RadioButton ThirdGr_fourth;
+
+    @FXML
+    private RadioButton SecondGr_fourth;
+
+    @FXML
+    private RadioButton FirstGr_fourth;
+
+    @FXML
+    private Button complete;
+
+    @FXML
+    void click(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,8 +11,9 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Запись апов");
+        primaryStage.setScene(new Scene(root, 903, 431));
+        primaryStage.setResizable(false);
         primaryStage.show();
     }
 
